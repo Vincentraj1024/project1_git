01e09b4a462b90ed4d576b80b8c7b761bf7d120a
Vincent Raj <ademvincent1024@gmail.com>
2024-12-04 06:34:44 +0000

    initial commit

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["project1git/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"project1_git/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
